98957c4caa6dcfd68b3d8ac1a9c8f2897099b6b9
Aayush shastri <aayush.25scs1003000664@iilm.edu>
2026-09-22 01:24:12 +0530

    Update display.java

--- ASSIGNMENT 8/display.java
+++ ASSIGNMENT 8/display.java
@@ -19,7 +19,7 @@ public class Main {
     public static void main(String[] args) {
         Display obj = new Display();
 
-        obj.show("Kushagra");
+        obj.show("aayush");
 
         System.out.println();
 
